a0713a98adbef6822d976a37081a29bec452301f
mauriciopinto <mauricio.pinto@utec.edu.pe>
2020-11-09 01:27:55 -0500

    bugs to fix for big splits

--- indexLanguages.h
+++ indexLanguages.h
@@ -1,13 +1,14 @@
 #include "BPage.h"
+#include <stack>
 
 // PageInfo contiene información del page (se usa como valor de retorno de findNodeByKey)
 struct PageInfo {
-	BPage parent;          // Padre del page 
-	int parentPosition;    // Posicion del page entre los hijos del padre
-	BPage current;         // Page actual
-	bool parentExists;     // true si hay un padre, false en caso contrario
-	int currentPosition;   // Posicion del page donde se encuentra el key
-	bool keyExists;		   // true si el key existe en el page, false si no existe
+	stack<pair<BPage, int>> parents;    // Padre del page 
+	int parentPosition;    				// Posicion del page entre los hijos del padre
+	BPage current;         				// Page actual
+	bool parentExists;     				// true si hay un padre, false en caso contrario
+	int currentPosition;   				// Posicion del page donde se encuentra el key
+	bool keyExists;		   				// true si el key existe en el page, false si no existe
 };
 
 pair<int, bool> binarySearch (BPage page, int left, int right, char key[]) {
@@ -54,9 +55,10 @@ public:
       char newKey[25];
 	  long long newDir;
 	  long long newOffset;
-
-	  while(file.peek() != EOF){
-        newDir = file.tellg();
+	  int n = 0;
+	  //while(file.peek() != EOF){
+	  for (int i = 0; i < 100; i++) {
+	  	newDir = file.tellg();
         
 		file >> newKey;
 		newDir = file.tellg ();
@@ -65,7 +67,8 @@ public:
 		newOffset = file.tellg() - newDir;
         newOffset--;
 		insert (newKey, newDir, newOffset, idioma, indexFile);
-    	}
+    	//}
+		}
 	}
 	void FindKey (char *key) {
 		ifstream index ("index.dat", ios::binary);
@@ -77,6 +80,7 @@ public:
 	PageInfo findNodeByKey (char key[]) {
 		ifstream index ("index.dat", ios::binary);
 		PageInfo info;
+		stack<pair<BPage, int>> parents;
 		root.read (index, root.pdir);
 
 		// Se busca el key en el root y se retorna la posicion donde debe ir y si se encuentra
@@ -89,19 +93,22 @@ public:
 			info.currentPosition = position.first;
 			info.keyExists = position.second;
 		}
-
+		
 		// Si el root no es hoja, se recorre el arbol hasta encontrar la hoja correspondiente
 		else {
 			BPage parentPage = root;
 			BPage nextPage;
-			nextPage.read (index, parentPage.children[position.first + 1]);
+			nextPage.keys = new Record[order + 1];
+			nextPage.children = new long long[order + 2];
+			nextPage.read (index, parentPage.children[position.first]);
 			while (!nextPage.isLeaf) {
-				parentPage = nextPage;
+				parents.push (make_pair (parentPage, position.first));
+				parentPage.read (index, nextPage.pdir);
 				position = binarySearch (parentPage, 0, parentPage.currentSize - 1, key);
-				nextPage.read (index, parentPage.children[position.first + 1]);
+				nextPage.read (index, parentPage.children[position.first]);
 			}
-			info.parent = parentPage;
-			info.parentPosition = position.first;
+			parents.push (make_pair (parentPage, position.first));
+			info.parents = parents;
 			info.parentExists = true;
 			info.current = nextPage;
 			position = binarySearch (nextPage, 0, nextPage.currentSize - 1, key);
@@ -117,19 +124,21 @@ public:
 		PageInfo info = findNodeByKey (key);
 
 		// Se extrae la información de los pares
-		BPage parent = info.parent;
-		int parentPosition = info.parentPosition;
+		pair<BPage, int> parentPair;
+		BPage parent;
+		int parentPosition;
 		bool parentExists = info.parentExists;
 		BPage current = info.current;
 		int currentPosition = info.currentPosition;
 		bool exists = info.keyExists;
-		
-		cout << "inserting key: " << key << endl;
-		cout << current.currentSize << ": ";
-		for (int i = 0; i <= order; i++) {
-			cout << current.keys[i].key << " ";
+		if (parentExists) {
+			parentPair = info.parents.top ();
+			parent = parentPair.first;
+			parentPosition = parentPair.second;
+			info.parents.pop ();
 		}
-		cout << endl;
+		
+		//cout << "inserting key: " << key << endl;
 
 		// Si existe el key, se debe revisar si ya existe una definicion en el idioma
 		// seleccionado.
@@ -157,28 +166,41 @@ public:
 			current.currentSize++;
 		}
 		
+		cout << "current node: |";
+		for (int i = 0; i < current.currentSize; i++)
+			cout << current.keys[i].key << " | ";
+		cout << endl;
 		// Si se sobrepasa la capacidad del page, se divide
 		if (current.currentSize > order)
-			splitLeaf (current, parent, parentPosition, parentExists, index);
-		current.write (index, current.pdir);
-		index.flush ();
-	}
-
-	void splitPage (BPage &current) {
-		cout << "Hasta aqui llegue" << endl;
-		return;
+			splitPage (current, parent, parentPosition, parentExists, index, info.parents);
+		else {
+			current.write (index, current.pdir);
+			index.flush ();
+		}
 	}
 	
-	void splitLeaf (BPage &current, BPage &parent, int parentPosition, bool parentExists, ofstream &index) {
+	void splitPage (BPage &current, BPage &parent, int parentPosition, bool parentExists, ofstream &index, stack<pair<BPage, int>> &parents) {
 		// Se calcula la posicion del key que se ascenderá y se crean los dos nuevos nodos
 		int midPosition = (order + 1) / 2;
 		BPage newPageRight (order);
 		BPage newPageLeft (order);
+		newPageRight.order = newPageLeft.order = order;
+		newPageRight.keys = new Record[order + 1];
+		newPageLeft.keys = new Record[order + 1];
+		newPageRight.children = new long long[order + 2];
+		newPageLeft.children = new long long[order + 2];
+		newPageLeft.isLeaf = newPageRight.isLeaf = current.isLeaf;
 		
 		// Si no hay parent, se crea un nuevo root
 		if (!parentExists) {
 			parent = BPage (order);
 			parentPosition = 0;
+			parent.pdir = 0;
+			parent.order = order;
+			parent.currentSize = 0;
+			parent.keys = new Record[order + 1];
+			parent.children = new long long[order + 2];
+			parent.isLeaf = false;
 		}
 		
 		// Se mueven los keys del parent para hacer espacio para la nueva key
@@ -193,20 +215,34 @@ public:
 		// Se mueven los children del parent para hacer espacio para el nuevo child
 		int j = i + 1;
 		while (j > parentPosition + 1) {
-			parent.children[i] = parent.children[i - 1];
-			i--;
+			parent.children[j] = parent.children[j - 1];
+			j--;
 		}
-
-		// Se copian los keys correspondientes a los nuevos nodos
+		
+		index.seekp (0, ios::end);
+		// Se copian los keys y children correspondientes a los nuevos pages
+		// Si el page dividido no es una hoja, se copian los children correspondientes
 		newPageLeft.pdir = index.tellp ();
-		for (int k = 0; k < midPosition; k++) {
+		for (int k = 0; k < midPosition; k++) { 
 			newPageLeft.keys[k] = current.keys[k];
+			newPageLeft.currentSize++;
+		}
+		
+		if (!current.isLeaf) {
+			for (int k = 0; k <= midPosition; k++)
+				newPageLeft.children[k] = current.children[k];
 		}
 		newPageLeft.write (index, newPageLeft.pdir);
 
 		newPageRight.pdir = index.tellp ();
 		for (int k = midPosition + 1; k < current.currentSize; k++) {
-			newPageRight.keys[k] = current.keys[k];
+			newPageRight.keys[k - (midPosition + 1)] = current.keys[k];
+			newPageRight.currentSize++;
+		}
+		
+		if (!current.isLeaf) {
+			for (int k = midPosition + 1; k < current.currentSize + 1; k++)
+				newPageRight.children[k - (midPosition + 1)] = current.children[k];
 		}
 		newPageRight.write (index, newPageRight.pdir);
 
@@ -216,11 +252,34 @@ public:
 		// Si no hay un padre, significa que se debe crear el nuevo root
 		if (!parentExists)
 			parent.pdir = 0;
+
+		cout << "New Parent: |";
+		for (int k = 0; k < parent.currentSize; k++)
+			cout << parent.keys[k].key << " | ";
+		cout << endl;
+		cout << "New Left Node: |";
+		for (int k = 0; k < newPageLeft.currentSize; k++)
+			cout << newPageLeft.keys[k].key << " | ";
+		cout << endl;
+		cout << "New Rigt Node: |";
+		for (int k = 0; k < newPageRight.currentSize; k++)
+			cout << newPageRight.keys[k].key << " | ";
+		cout << endl;
 		parent.write (index, parent.pdir);
 		index.flush ();
-
+		if (parent.pdir == 0) {
+			index.seekp (0, ios::end);
+		}
+		int finalPos = index.tellp ();
 		// Si el padre excede el tamaño máximo, se divide
-		if (parent.currentSize > order)
-			splitPage (parent);
+		if (parent.currentSize > order) {
+			parentExists = parents.size () > 0 ? true : false;
+			pair<BPage, int> parentPair;
+			if (parentExists) {
+				parentPair = parents.top ();
+				parents.pop ();
+			}
+			splitPage (parent, parentPair.first, parentPair.second, parentExists, index, parents);
+		}
 	}
 };
